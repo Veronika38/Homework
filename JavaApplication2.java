dbf1300e0044f0287f7c7cf9f1849c617da38f8e
Veronika38 <92428119+Veronika38@users.noreply.github.com>
2021-10-17 22:54:42 +0800

    Add files via upload
    First_Homework

---
+++ JavaApplication2.java
@@ -0,0 +1,29 @@
+package javaapplication2;
+/**
+ * Program's output are numbers from 1 to 100,
+ * if one from it mod 3==0 --> OUTPUT="Fizz",
+ * if it mod 5==0 --> OUTPUT="Buzz",
+ * if it mod 3==0 AND it mod 5==0 --> OUTPUT="FizzBuzz"
+ * @author VERONIKA
+ */
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+        //Tracking from 1 to 100
+        for(int i=1; i<101; i++) {
+            if (i%15==0){
+                System.out.println("FizzBuzz"); //Condition for mod 5 and 3, we can put together as 15
+                continue; //Use continue to stop other comparisons
+            }
+            if (i%3==0){
+                System.out.println("Fizz"); //Condition for mod 3
+                continue; 
+            }
+            if (i%5==0){
+                System.out.println("Buzz"); //Condition for mod 5
+                continue;
+            }
+            System.out.println(i); //If other comparisons didn't work out println number itself
+        }
+    } 
+}
